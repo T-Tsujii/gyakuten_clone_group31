ef9c0980d7d2ae9e8e53a2151512fc1ca724b68a
Yumiko Tezuka <ytezuka5@gmail.com>
2020-09-26 12:13:48 +0900

    movie_idカラムの削除

--- db/migrate/20200926012045_create_movies.rb
+++ db/migrate/20200926012045_create_movies.rb
@@ -3,8 +3,6 @@ class CreateMovies < ActiveRecord::Migration[6.0]
     create_table :movies do |t|
       t.string :title
       t.string :url
-      t.integer :movie_id
-
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_012045) do
+ActiveRecord::Schema.define(version: 2020_09_26_025641) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2020_09_26_012045) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "url"
-    t.integer "movie_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
