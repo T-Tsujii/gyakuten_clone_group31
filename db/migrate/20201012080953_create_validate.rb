61ee9535e3cdfc8f921943aa06f12302068b77a3
SHINICHI <masaki.shinichi.1984@gmail.com>
2020-10-12 23:06:47 +0900

    マイグレーションファイル2つ追加

--- db/migrate/20201012080953_create_validate.rb
+++
@@ -1,9 +0,0 @@
-class CreateValidate < ActiveRecord::Migration[6.0]
-  def change
-    create_table :validates do |t|
-      change_column_null :title, false
-      change_column_null :content, false
-      change_column_null :url, false
-    end
-  end
-end
