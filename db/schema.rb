9ca5362f3f6fcd454483307f0c4e33bb4fd648ed
ajk0421 <ajk.spirituallounge@gmai.com>
2020-10-12 22:16:24 +0900

    change_colmn_movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_26_054623) do
+ActiveRecord::Schema.define(version: 2020_10_12_223545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 2020_09_26_054623) do
   end
 
   create_table "movies", force: :cascade do |t|
-    t.string "title"
-    t.string "url"
+    t.string "title", null: false
+    t.string "url", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
